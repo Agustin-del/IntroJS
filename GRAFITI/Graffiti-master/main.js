b285851517465f461a386649bcbbf0fca5faff6e
Agustin-del <eddyruveda@gmail.com>
2024-04-10 09:53:34 -0300

    fin

--- GRAFITI/Graffiti-master/main.js
+++ GRAFITI/Graffiti-master/main.js
@@ -1,33 +1,37 @@
 let contenedor = document.getElementById("formMain")
 let poster = document.getElementById("poster")
-let text = document.getElementById("texto")
-let colour = document.getElementById("color")
 
 contenedor.addEventListener("submit", (event) => {
+    
     event.preventDefault();
-    const { color, texto } = event.target;
+    const {color, texto, inputFont} = event.target;
     let grafitiPoster = document.createElement('div');
     grafitiPoster.classList.add("text-xl","break-all", `w-[300px]`, `h-[300px]`, `bg-[${color.value}]`, "p-4");
-    let text = document.createElement('h2')
+    let text = document.createElement('h2');
     text.innerText = texto.value;
-    text.className = "flex items-center w-full flex-wrap"
+    text.className = "flex items-center w-full flex-wrap";
     
-    let fuente = document.getElementById('inputFont')
-    if (fuente.checked) {
-        grafitiPoster.classList.add('italic')
+    for (const element of inputFont) {
+        if (element.checked && element.value ==='grafitty') {
+           grafitiPoster.classList.add('italic'); 
+        }
     }
+    // let fuente = document.getElementById('inputFont')
+    // if (fuente.checked) {
+    //     grafitiPoster.classList.add('italic')
+    // }
 
     contenedor.reset()
-    let closeButton = document.createElement('button');
-    closeButton.classList.add("h-[30px]", "object-fit", "w-full", "text-right");
-    closeButton.textContent = "X";  
+    
+    let closeButton = document.createElement('span');
+    closeButton.classList.add("h-[30px]", "flex", "w-full", "justify-end");
+    closeButton.innerHTML = `<img src="./cross.svg" class ="h-[20px]">`;  
     closeButton.addEventListener("click", (event) => {
         event.preventDefault();
         grafitiPoster.remove();
     });
     
     grafitiPoster.appendChild(closeButton);
-
     poster.appendChild(grafitiPoster);
     grafitiPoster.appendChild(text)
 });
